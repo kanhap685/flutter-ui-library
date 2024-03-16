96c38ec3a582064af43a11bdff14ab08da2dcf7d
kanha.P <kanha@theprodigy.biz>
2024-03-17 00:50:15 +0700

    add UiHeader

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Storybook(
-      initialStory: 'UiBottomSheetxx',
+      initialStory: 'UiHeader',
       plugins: [
         ThemeModePlugin(initialTheme: ThemeMode.light),
       ],
@@ -638,6 +638,74 @@ class MyApp extends StatelessWidget {
                     },
                     child: const Text('Show Ui Modal'),
                   ),
+                ],
+              ),
+            ),
+          ),
+        ),
+
+        /// ##################### Example UI Modal #####################
+        Story(
+          name: 'UiHeader',
+          builder: (context) => Scaffold(
+            body: SizedBox(
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(16.0),
+                    color: Colors.blue,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            // Add your back icon functionality here
+                          },
+                          icon: const Icon(
+                            Icons.arrow_back,
+                            color: Colors.white,
+                            size: 24.0,
+                          ),
+                        ),
+                        const Text(
+                          "title",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            IconButton(
+                              onPressed: () {
+                                // Add your add icon functionality here
+                              },
+                              icon: const Icon(
+                                Icons.add,
+                                color: Colors.white,
+                                size: 24.0,
+                              ),
+                            ),
+                            const SizedBox(width: 8),
+                            IconButton(
+                              onPressed: () {
+                                // Add your add icon functionality here
+                              },
+                              icon: const Icon(
+                                Icons.delete,
+                                color: Colors.white,
+                                size: 24.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
